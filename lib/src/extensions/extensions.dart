2b7b019c69bdf63387848febf9ddb05acbb72cd6
Bruno Bastiani <ba.bastiani@gmail.com>
2025-04-30 20:19:55 -0300

    Update extensions.dart

--- lib/src/extensions/extensions.dart
+++ lib/src/extensions/extensions.dart
@@ -81,7 +81,7 @@ extension ValidateString on String {
     return true;
   }
 
-  bool get isUrl => Uri.tryParse(this)?.isAbsolute ?? false;
+  bool get isUrl => Uri.tryParse(this)?.isAbsolute && ! startsWith('data:image') ?? false;
 
   Widget getUserProfilePicture({
     required ChatUser? Function(String) getChatUser,
